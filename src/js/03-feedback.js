e2839e64672d1f8f27c126e061155ce53422979c
Mykola Kovalskyi <112631611+mykolakovalskyi@users.noreply.github.com>
2023-03-07 22:41:07 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input');
+const massageInput = form.querySelector('textarea');
+const throttle = require('lodash.throttle');
 
+let savedFormData = localStorage.getItem('feedback-form-state');
+let formData = {};
+
+try {
+  if (savedFormData !== null) {
+    formData = JSON.parse(savedFormData);
+  }
+} catch (error) {
+  console.log(error.name);
+  console.log(error.message);
+}
+
+if (formData.email !== undefined) {
+  emailInput.value = formData.email;
+}
+if (formData.message !== undefined) {
+  massageInput.value = formData.message;
+}
+
+form.addEventListener(
+  'input',
+  throttle(event => {
+    if (event.target.nodeName === 'INPUT') {
+      formData.email = event.target.value;
+    }
+    if (event.target.nodeName === 'TEXTAREA') {
+      formData.message = event.target.value;
+    }
+    let jsonFormData = JSON.stringify(formData);
+    localStorage.setItem('feedback-form-state', jsonFormData);
+  }, 500)
+);
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  if (emailInput.value !== '' && massageInput.value !== '') {
+    event.currentTarget.reset();
+    localStorage.clear();
+    console.log(formData);
+  }
+});
