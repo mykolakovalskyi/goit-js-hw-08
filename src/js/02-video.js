137428f920ffe1405c7842a87ea568e7a0f46456
Mykola Kovalskyi <112631611+mykolakovalskyi@users.noreply.github.com>
2023-03-07 20:53:34 +0100

    02-video.js update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+player.setCurrentTime(savedTime);
+
+player.on(
+  'timeupdate',
+  throttle(currentTime => {
+    localStorage.setItem(
+      'videoplayer-current-time',
+      JSON.stringify(currentTime.seconds)
+    );
+  }, 1000)
+);
